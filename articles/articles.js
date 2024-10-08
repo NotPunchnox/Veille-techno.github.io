7700321886ffada1b8b5be34637898cb58e39b83
NotPunchnox <punchnoxpaypal@gmail.com>
2024-10-08 16:45:02 +0200

    update articlesDB

--- articles/articles.js
+++ articles/articles.js
@@ -1,3 +1,5 @@
+const link = "https://notpunchnox.github.io/Veille-techno.github.io/articles/images/"
+
 export default {
 
     "AI": [{
@@ -17,11 +19,11 @@ export default {
                 "<h3>Disponibilité</h3><p>Llama 3.2 sera disponible gratuitement sur <strong>WhatsApp</strong>, <strong>Instagram</strong>, <strong>Facebook</strong>, et <strong>Messenger</strong>. Toutefois, les versions multimodales ne seront pas accessibles en Europe tant que Meta n’aura pas résolu les questions de conformité avec les régulations européennes.</p>",
             ],
             "images": [{
-                    "url": "/articles/images/Benchmarks-llama3.2.png",
+                    "url": link + "Benchmarks-llama3.2.png",
                     "caption": "Llama devient multimodal et peut travailler avec des images"
                 },
                 {
-                    "url": "/articles/images/SchemaLlama.png",
+                    "url": link + "SchemaLlama.png",
                     "caption": "Llama Schema"
                 }
             ],
@@ -29,11 +31,40 @@ export default {
                 "title": "Frandroid",
                 "url": "https://www.frandroid.com/marques/meta/2349720_llama-3-2-meta-met-a-nouveau-les-bouchees-doubles-en-matiere-dintelligence-artificielle"
             }]
-        }
+        },
 
     ],
 
     ROBOTIC: [{
+        "title": "Un nouveau robot humanoïde chez Boston Dynamics",
+        "description": "Boston Dynamics a récemment présenté une version entièrement électrique de son robot bipède Atlas.",
+        "date": "30 septembre 2024",
+        "content": ["<h3>Une nouvelle version tout électrique du robot bipède Atlas</h3>",
+            "<p>Boston Dynamics a récemment présenté une version entièrement électrique de son célèbre robot bipède Atlas, marquant ainsi la fin de l'ère de l’actionnement hydraulique. Cette révolution technologique permet d’offrir une plus grande amplitude de mouvements tout en simplifiant la conception.</p>",
+            "<h3>Fin de l'actionnement hydraulique</h3>",
+            "<p>L’une des annonces majeures de cette nouvelle version est l’abandon du système d'actionnement hydraulique, autrefois utilisé dans les modèles précédents d'Atlas.</p>",
+            "<h3>Caractéristiques techniques du nouveau modèle</h3>",
+            "<ul><li><strong>Amplitude de mouvement augmentée</strong> : Grâce à l’électricité, Atlas peut réaliser des mouvements plus fluides.</li><li><strong>Absence d'acrobaties</strong> : Ce nouvel Atlas ne pourra pas réaliser d'acrobaties spectaculaires.</li><li><strong>Simplification de la programmation</strong> : Le contrôle du robot est désormais plus intuitif.</li></ul>",
+            "<h3>Objectif : Applications concrètes</h3>",
+            "<p>Le but de Boston Dynamics est désormais de commercialiser Atlas en l'adaptant à des applications réelles.</p>",
+            "<p><a href=\"https://bostondynamics.com/atlas/?wvideo=p7iw5vfjrz\"><img src=\"https://embed-ssl.wistia.com/deliveries/2368ba4968169ffed2b4fccc9a0c01a7.jpg?image_play_button_size=2x&amp;image_crop_resized=960x540&amp;image_play_button=1&amp;image_play_button_color=00355ae0\" width=\"400\" height=\"225\" style=\"width: 400px; height: 225px;\"></a></p><p><a href=\"https://bostondynamics.com/atlas/?wvideo=p7iw5vfjrz\">Atlas | Boston Dynamics</a></p>"
+        ],
+        "images": [{
+            "url": link + "Atlas.jpg",
+            "caption": "Image du robot Atlas"
+        }],
+        "sources": [{
+                "title": "Boston Dynamics Officiel",
+                "url": "https://bostondynamics.com/atlas"
+            },
+            {
+                "title": "Koreus Vidéo : Atlas en action",
+                "url": "https://www.koreus.com/embed/boston-dynamics-new-atlas"
+            }
+        ]
+    },],
+
+    CYBER: [{
         "title": "Nvidia corrige une vulnérabilité critique dans son Container Toolkit",
         "description": "Nvidia a récemment corrigé une vulnérabilité critique dans son outil de conteneurisation, Container Toolkit.",
         "date": "30 septembre 2024",
@@ -46,42 +77,14 @@ export default {
             "<p>Nvidia a publié un correctif pour cette vulnérabilité. Tous les utilisateurs de Nvidia Container Toolkit sont invités à mettre à jour vers la version la plus récente.</p>"
         ],
         "images": [{
-            "url": "/articles/images/faille-nvidia.png",
+            "url": link + "faille-nvidia.png",
             "caption": "Image de Wiz Research - vulnérabilité Nvidia"
         }],
         "sources": [{
             "title": "Le Monde Informatique",
             "url": "https://www.lemondeinformatique.fr/actualites/lire-docker-augmente-le-prix-de-ses-abonnements-pro-et-team-94719.html"
         }]
-    }],
-
-    CYBER: [{
-        "title": "Un nouveau robot humanoïde chez Boston Dynamics",
-        "description": "Boston Dynamics a récemment présenté une version entièrement électrique de son robot bipède Atlas.",
-        "date": "30 septembre 2024",
-        "content": ["<h3>Une nouvelle version tout électrique du robot bipède Atlas</h3>",
-            "<p>Boston Dynamics a récemment présenté une version entièrement électrique de son célèbre robot bipède Atlas, marquant ainsi la fin de l'ère de l’actionnement hydraulique. Cette révolution technologique permet d’offrir une plus grande amplitude de mouvements tout en simplifiant la conception.</p>",
-            "<h3>Fin de l'actionnement hydraulique</h3>",
-            "<p>L’une des annonces majeures de cette nouvelle version est l’abandon du système d'actionnement hydraulique, autrefois utilisé dans les modèles précédents d'Atlas.</p>",
-            "<h3>Caractéristiques techniques du nouveau modèle</h3>",
-            "<ul><li><strong>Amplitude de mouvement augmentée</strong> : Grâce à l’électricité, Atlas peut réaliser des mouvements plus fluides.</li><li><strong>Absence d'acrobaties</strong> : Ce nouvel Atlas ne pourra pas réaliser d'acrobaties spectaculaires.</li><li><strong>Simplification de la programmation</strong> : Le contrôle du robot est désormais plus intuitif.</li></ul>",
-            "<h3>Objectif : Applications concrètes</h3>",
-            "<p>Le but de Boston Dynamics est désormais de commercialiser Atlas en l'adaptant à des applications réelles.</p>"
-        ],
-        "images": [{
-            "url": "/articles/images/Atlas.jpg",
-            "caption": "Image du robot Atlas"
-        }],
-        "sources": [{
-                "title": "Boston Dynamics Officiel",
-                "url": "https://bostondynamics.com/atlas"
-            },
-            {
-                "title": "Koreus Vidéo : Atlas en action",
-                "url": "https://www.koreus.com/embed/boston-dynamics-new-atlas"
-            }
-        ]
-    }],
+    }, ],
 
     DEV: []
 
